367c8513e2d75e2f7241c6263f20178b5018e989
Pieter Maes <pieter@spentys.com>
2023-12-13 14:54:41 +0000

    Force std_impl unordered implementation

--- include/carve/collection/unordered.hpp
+++ include/carve/collection/unordered.hpp
@@ -16,28 +16,28 @@
 
 #pragma once
 
-#if defined(HAVE_STD_UNORDERED_COLLECTIONS)
+// #if defined(HAVE_STD_UNORDERED_COLLECTIONS)
 
 #  include <carve/collection/unordered/std_impl.hpp>
 
-#elif defined(HAVE_TR1_UNORDERED_COLLECTIONS)
+// #elif defined(HAVE_TR1_UNORDERED_COLLECTIONS)
 
-#  include <carve/collection/unordered/tr1_impl.hpp>
+// #  include <carve/collection/unordered/tr1_impl.hpp>
 
-#elif defined(HAVE_BOOST_UNORDERED_COLLECTIONS)
+// #elif defined(HAVE_BOOST_UNORDERED_COLLECTIONS)
 
-#  include <carve/collection/unordered/boost_impl.hpp>
+// #  include <carve/collection/unordered/boost_impl.hpp>
 
-#elif defined(HAVE_LIBSTDCPP_UNORDERED_COLLECTIONS)
+// #elif defined(HAVE_LIBSTDCPP_UNORDERED_COLLECTIONS)
 
-#  include <carve/collection/unordered/libstdcpp_impl.hpp>
+// #  include <carve/collection/unordered/libstdcpp_impl.hpp>
 
-#elif defined(_MSC_VER) && _MSC_VER >= 1300
+// #elif defined(_MSC_VER) && _MSC_VER >= 1300
 
-#  include <carve/collection/unordered/vcpp_impl.hpp>
+// #  include <carve/collection/unordered/vcpp_impl.hpp>
 
-#else
+// #else
 
-#  include <carve/collection/unordered/fallback_impl.hpp>
+// #  include <carve/collection/unordered/fallback_impl.hpp>
 
-#endif
+// #endif
